18bc5f8ba53495161c3298ec3d975dd20ceb7a89
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-08-01 19:12:21 +0300

    refactor

--- main.c
+++ main.c
@@ -11,24 +11,25 @@ int main()
 
     printf("Write (C) for Celsius to Fahrenheit or (F) for Fahrenheit to Celsius (F): ");
     scanf(" %c", &unit);
-    if (unit == 'C' || unit == 'c')
+    switch (unit)
     {
+    case 'C':
+    case 'c':
         printf("Enter temperature in Celsius: ");
         scanf("%f", &temperature);
         convertedTemperature = (temperature * 9.0 / 5.0) + 32.0;
         printf("Temperature in Celsius: %.1f Celsius\n", temperature);
         printf("Temperature in Fahrenheit: %.1f Fahrenheit\n", convertedTemperature);
-    }
-    else if (unit == 'F' || unit == 'f')
-    {
+        break;
+    case 'F':
+    case 'f':
         printf("Enter temperature in Fahrenheit: ");
         scanf("%f", &temperature);
         convertedTemperature = (temperature - 32.0) * 5.0 / 9.0;
         printf("Temperature in Fahrenheit: %.1f Fahrenheit\n", temperature);
         printf("Temperature in Celsius: %.1f Celsius\n", convertedTemperature);
-    }
-    else
-    {
+        break;
+    default:
         printf("Invalid input! Please enter 'C' for Celsius or 'F for Fahrenheit!\n");
         main();
     }
@@ -36,15 +37,16 @@ int main()
     int choice = 0;
     printf("To exit the program enter (0), and to buy another value enter other number: ");
     scanf("%d", &choice);
-    if (choice == 0)
+    switch (choice)
     {
+    case 0:
         printf("Thank you for using the Temperature Converter program. Goodbye!\n");
-        return 0;
-    }
-    else
-    {
+        break;
+    default:
         printf("\n\n");
         getchar();
         main();
     }
+
+    return 0;
 }
